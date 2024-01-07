7ac81a85ced05eff5d37e65c11858074afce7171
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-07 15:37:53 +0000

    flashcard container component started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import FlashcardContainer from "./FlashcardContainer";
 
 function App() {
   return (
     <>
       <Header />
+      <FlashcardContainer />
       <Footer />
     </>
   );
