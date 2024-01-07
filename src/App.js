811c095b5c4e32b7d5ebc51273b213ebc006b727
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-07 15:57:18 +0000

    re-did so only one flashcard folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
-import FlashcardContainer from "./FlashcardContainer";
 
 function App() {
   return (
     <>
       <Header />
-      <FlashcardContainer />
       <Footer />
     </>
   );
