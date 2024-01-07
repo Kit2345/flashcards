be0a8c4fdee736394a2d117bd769eccfe3f2cafc
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-07 14:59:02 +0000

    header done

---
+++ flashcards/src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <img src="logo192.png" alt="logo" width="50px" />
+      <h1>React Flashcards</h1>
+      <p>🧠 Expand Your React Knowledge, One Flashcard at a Time! 🐌</p>
+    </header>
+  );
+}
+
+export default Header;
