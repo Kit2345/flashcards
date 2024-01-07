f808f0f26176a7ef95a7e8dc874a40cc767e9a34
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-07 16:09:04 +0000

    flashcards started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import FlashcardsContainer from "./FlashcardsContainer";
 
 function App() {
   return (
     <>
       <Header />
+      <FlashcardsContainer />
       <Footer />
     </>
   );
---
+++ src/FlashcardsContainer.js
@@ -0,0 +1,7 @@
+export default function FlashcardsContainer() {
+  return (
+    <>
+      <p>this works</p>
+    </>
+  );
+}
